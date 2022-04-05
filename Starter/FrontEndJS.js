e1bb4e43b31e6649e4841a45e4eacb399df7cbb5
Jawad Kadri <jawadskadri@gmail.com>
2022-04-05 04:30:08 -0400

    Modified Header/Nav Bar/Footer/Mobile Nav

--- Starter/FrontEndJS.js
+++ Starter/FrontEndJS.js
@@ -4,5 +4,12 @@
 //For Desktop
 
 //For Mobile
+/* Display the dropdown when the nav bar is clicked in mobile view */
+$(document).ready(function(){
+    $(' .menu-toggle').on('click', function() {
+        $('.nav').toggleClass('showing');       // Everything in the NAV should appear on click
+        $('.nav ul').toggleClass('showing');    // NAV + UL should appear on click
+    });
+});
 
 //Giving User-based Privileges -> add to the backend.
