2d0a4321ef1f19f9e09ce8513c0df739b1408f82
InfluencedNerd <74060414+InfluencedNerd@users.noreply.github.com>
2022-04-07 17:10:43 -0400

    added the accordion

--- Starter/FrontEndJS.js
+++ Starter/FrontEndJS.js
@@ -11,3 +11,19 @@ $(document).ready(function(){
     });
 });
 
+//For the accordian in the Base Template
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[j].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } 
+            else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+}
