4a512128f4df4fa5004e12206bb58becb0762839
Jawad Kadri <jawadskadri@gmail.com>
2022-04-08 12:17:38 -0400

    Create the db and tables if empty

--- database/connection.php
+++ database/connection.php
@@ -2,47 +2,45 @@
     $DATABSE_HOST = "localhost";
     $DATABSE_USER = "root";
     $DATABSE_PSW = "";
-    $DATABASE_NAME = "gi_db";
+    $DATABASE_NAME = "";
 
-    $connection = new MYSQLi($DATABSE_HOST, $DATABSE_USER, $DATABSE_PSW, $DATABASE_NAME);
-    
-    if(!$connection) {
-        echo "Connection failed";
-        die("Connection failed: " . mysqli_connect_error());
-    }
+    $connection = mysqli_connect($DATABSE_HOST, $DATABSE_USER, $DATABSE_PSW, $DATABASE_NAME);
 
-    // Create database
-    $sql = "CREATE DATABASE IF NOT EXISTS gi_db";
-    if(mysqli_query($connection, $sql)) {
-        $sql = "CREATE TABLE IF NOT EXISTS buisnesses (
-            id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            companyName VARCHAR(50) NOT NULL,
-            email VARCHAR(50) NOT NULL,
-            password VARCHAR(255) NOT NULL
-        )";
+     // Create database
+     $sql = "CREATE DATABASE IF NOT EXISTS gi_db";  // Create the database
+     $DATABASE_NAME = "gi_db";  // Set the database name
+     if(mysqli_query($connection, $sql)) {  // If the database was created successfully
+         $sql = "CREATE TABLE IF NOT EXISTS gi_db.buisnesses (  
+             id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+             companyName VARCHAR(50) NOT NULL,
+             email VARCHAR(50) NOT NULL,
+             password VARCHAR(255) NOT NULL
+         )";    // Create the table
+ 
+         if(mysqli_query($connection, $sql)) {
+             //echo "Found Table buisnesses successfully";
+         } else {
+             echo "Error finding table: " . mysqli_error($connection);
+         }
+ 
+         $sql = "CREATE TABLE IF NOT EXISTS gi_db.users (   
+             id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+             firstname VARCHAR(50) NOT NULL,
+             lastname VARCHAR(50) NOT NULL,
+             email VARCHAR(50) NOT NULL,
+             password VARCHAR(255) NOT NULL,
+             creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+             permission TINYINT(3) NOT NULL
+         )";    // Create the table
+ 
+         if(mysqli_query($connection, $sql)) {
+             //echo "Found Table users successfully";
+         } else {
+             echo "Error finding table: " . mysqli_error($connection);
+         }
+     } else {
+         echo "Error finding database: " . mysqli_error($connection);
+     }
 
-        if(mysqli_query($connection, $sql)) {
-            //echo "Found Table buisnesses successfully";
-        } else {
-            echo "Error finding table: " . mysqli_error($connection);
-        }
-
-        $sql = "CREATE TABLE IF NOT EXISTS users (
-            id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            firstname VARCHAR(50) NOT NULL,
-            lastname VARCHAR(50) NOT NULL,
-            email VARCHAR(50) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            permission TINYINT(3) NOT NULL
-        )";
-
-        if(mysqli_query($connection, $sql)) {
-            //echo "Found Table users successfully";
-        } else {
-            echo "Error finding table: " . mysqli_error($connection);
-        }
-    } else {
-        echo "Error finding database: " . mysqli_error($connection);
-    }
+     $connection = new MYSQLi($DATABSE_HOST, $DATABSE_USER, $DATABSE_PSW, $DATABASE_NAME) or die("Error: " . mysqli_error($connection));
 ?>
