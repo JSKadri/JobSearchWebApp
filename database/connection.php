452e9f5d6923deaf2327327caff4c7297bb01c9e
Jawad Kadri <jawadskadri@gmail.com>
2022-04-08 11:11:09 -0400

    Create db and tables if not found

--- database/connection.php
+++ database/connection.php
@@ -9,8 +9,40 @@
     if(!$connection) {
         echo "Connection failed";
         die("Connection failed: " . mysqli_connect_error());
-        // In the future we will redirect to a page that will tell the user that there was an error.
+    }
+
+    // Create database
+    $sql = "CREATE DATABASE IF NOT EXISTS gi_db";
+    if(mysqli_query($connection, $sql)) {
+        $sql = "CREATE TABLE IF NOT EXISTS buisnesses (
+            id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            companyName VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            password VARCHAR(255) NOT NULL
+        )";
+
+        if(mysqli_query($connection, $sql)) {
+            //echo "Found Table buisnesses successfully";
+        } else {
+            echo "Error finding table: " . mysqli_error($connection);
+        }
+
+        $sql = "CREATE TABLE IF NOT EXISTS users (
+            id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(50) NOT NULL,
+            lastname VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            permission TINYINT(3) NOT NULL
+        )";
+
+        if(mysqli_query($connection, $sql)) {
+            //echo "Found Table users successfully";
+        } else {
+            echo "Error finding table: " . mysqli_error($connection);
+        }
     } else {
-       // echo "Connected successfully";
+        echo "Error finding database: " . mysqli_error($connection);
     }
 ?>
